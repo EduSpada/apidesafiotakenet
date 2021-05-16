1d7b28598ea9319c24a9643cc7281e7c88134602
EduSpada <73047396+EduSpada@users.noreply.github.com>
2021-05-16 18:02:40 -0300

    Alterado para pegar dados da lista
    Realizado alteração

--- api/takenet.js
+++ api/takenet.js
@@ -1,10 +1,10 @@
 function takenet(request, response) {
     //const dynamicDate = new Date();
     const dadosTake = await fetch("https://api.github.com/users/takenet/repos");
-    //const dadosJson = await dadosTake.json();
+    const dadosJson = await dadosTake[1].json();
 
     response.json({
-        dados: dadosTake
+        dados: dadosJson
 
     })
 }
