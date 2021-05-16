bbe59e75c1ae15ab618f869a6d2dce9aa5421338
EduSpada <73047396+EduSpada@users.noreply.github.com>
2021-05-16 18:40:12 -0300

    Ajuste na API
    Ajuste

--- api/index.js
+++ api/index.js
@@ -1,11 +1,39 @@
 function takenet(request, response) {
-    const dynamicDate = new Date();
+    //const dynamicDate = new Date();
     //const dadosTake = await fetch("https://api.github.com/users/takenet/repos");
     //const dadosJson = await dadosTake.json();
 
     response.json({
-        date: dynamicDate
-
-    })
+        "94437568": {
+          "descripition": "The C# SDK for the blip.ai bot platform",
+          "name": "takenet/blip-sdk-csharp",
+          "avatar": "https://avatars.githubusercontent.com/u/4369522?v=4",
+          "created_at": "2017-06-15T12:32:37Z"
+        },
+        "95885876": {
+          "descripition": "A host to create acceptance tests for chatbots created using BLiP C# SDK (https://github.com/takenet/messaginghub-client-csharp)",
+          "name": "takenet/blip-client-testing-csharp",
+          "avatar": "https://avatars.githubusercontent.com/u/4369522?v=4",
+          "created_at": "2017-06-30T12:20:58Z"
+        },
+        "104798298": {
+          "descripition": "Samples for the Take's C# async coding training",
+          "name": "takenet/async-samples",
+          "avatar": "https://avatars.githubusercontent.com/u/4369522?v=4",
+          "created_at": "2017-09-25T20:23:09Z"
+        },
+        "105922024": {
+          "descripition": "Some useful tasks for your bot",
+          "name": "takenet/blip-task-utils",
+          "avatar": "https://avatars.githubusercontent.com/u/4369522?v=4",
+          "created_at": "2017-10-05T17:54:54Z"
+        },
+        "127461205": {
+          "descripition": "BLiP command-line (CLI) tool, used for managing data of BLiP's bots. blip-CLI helps to make easy and quickly your development flow and the execution of some repetitive tasks.",
+          "name": "takenet/blip-CLI",
+          "avatar": "https://avatars.githubusercontent.com/u/4369522?v=4",
+          "created_at": "2018-03-30T18:38:55Z"
+        }
+      })
 }
 export default takenet;
