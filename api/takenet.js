45478aeac5ed8836cb8fcfbd6da00e34df6fb5f6
EduSpada <73047396+EduSpada@users.noreply.github.com>
2021-05-16 17:58:30 -0300

    Removido a alteração para json
    Realizada a alteração pois o o get está retornando uma lista

--- api/takenet.js
+++ api/takenet.js
@@ -1,10 +1,10 @@
 function takenet(request, response) {
     //const dynamicDate = new Date();
     const dadosTake = await fetch("https://api.github.com/users/takenet/repos");
-    const dadosJson = await dadosTake.json();
+    //const dadosJson = await dadosTake.json();
 
     response.json({
-        dados: dadosJson
+        dados: dadosTake
 
     })
 }
