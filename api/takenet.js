2666fbaedbd290d4debb845ace3eb084e5670d96
EduSpada <73047396+EduSpada@users.noreply.github.com>
2021-05-16 17:47:48 -0300

    Modificando a API
    Agora os dados da API são os mesmos do repositório da TAKE

--- api/takenet.js
+++ api/takenet.js
@@ -1,8 +1,11 @@
 function takenet(request, response) {
-    const dynamicDate = new Date();
+    //const dynamicDate = new Date();
+    const dadosTake = await fetch("https://api.github.com/users/takenet/repos");
+    const dadosJson = await dadosTake.json();
 
     response.json({
-        date: dynamicDate.toGMTString()
+        dados: dadosJson
+
     })
 }
 export default takenet;
